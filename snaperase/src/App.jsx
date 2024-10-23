814f5302151a17f67d0a4cd3c9f9790299e8e47c
Darshan7573 <darshu7573@gmail.com>
2024-10-23 12:46:56 +0530

    routes added for pages

--- snaperase/src/App.jsx
+++ snaperase/src/App.jsx
@@ -1,6 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/result' element={<Result />} />
+        <Route path='/buy-credit' element={<BuyCredit />} />
+      </Routes>
+    </div>
   )
 }
 
